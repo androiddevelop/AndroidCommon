b96cd7e4e65d992f57969866053ea8b0f197cae8
yuedong <app@codeboy.me>
2016-12-16 00:17:00 +0800

    update hander

--- src/main/java/me/codeboy/android/common/component/CBHandler.java
+++ src/main/java/me/codeboy/android/common/component/CBHandler.java
@@ -1,5 +1,7 @@
 package me.codeboy.android.common.component;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 
@@ -10,27 +12,40 @@ import me.codeboy.android.common.app.CBActivity;
 /**
  * Created by yuedong.lyd on 6/7/15.
  * <p>
- *    构建防止内存泄露的handler
+ * 构建防止内存泄露的handler
  * </p>
  */
 public class CBHandler {
     /**
      * 防止handler对activity有隐式引用，匿名内部类不会对外部类有引用
      */
-   public static class UnleakHandler extends Handler {
-        private final WeakReference<CBActivity> activity;
+    public static class UnleakHandler extends Handler {
+        private final WeakReference<CBActivity> mActivity;
 
         public UnleakHandler(CBActivity activity) {
-            this.activity =  new WeakReference<CBActivity>(activity);
+            this.mActivity = new WeakReference<CBActivity>(activity);
+        }
+
+        @Override
+        public void dispatchMessage(Message msg) {
+            Activity activity = mActivity.get();
+            if (activity != null && !activity.isFinishing()) {
+                if (Build.VERSION.SDK_INT >= 17) {
+                    if (activity.isDestroyed()) {
+                        return;
+                    }
+                }
+                super.dispatchMessage(msg);
+            }
         }
 
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if(activity.get() == null) {
+            if (mActivity.get() == null) {
                 return;
             }
-            activity.get().processMessage(msg);
+            mActivity.get().processMessage(msg);
         }
     }
 }
